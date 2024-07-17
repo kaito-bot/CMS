579b3cfc256b997670be3fa697f44b4b02e7d093
Karthik-Divakaran <Karthik.Divakaran@ibm.com>
2024-07-17 16:24:12 +0530

    commit 1

--- src/Components/login/Login.component.jsx
+++ src/Components/login/Login.component.jsx
@@ -1,45 +1,56 @@
 import React, { useState } from 'react';
-import "./login.css"
+import { useNavigate } from 'react-router-dom';
+import './login.css';
+import Dashboard from '../../Pages/Dashboard.component';
+
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add your login logic here
-    if (email === "test@example.com" && password === "password") {
-      alert("Login successful!");
+    if (email === 'test@example.com' && password === 'password') {
+      alert('Login successful!');
+      navigate('/dashboard/*');
     } else {
-      setErrorMessage("Invalid email or password");
+      setErrorMessage('Invalid User ID or password');
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-        <button type="submit">Login</button>
-      </form>
+    <div className="login-page">
+      <div className="logo-container">
+        <img src="college-logo.png" alt="College Logo" />
+      </div>
+      <div className="login-container">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>UserID:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          {errorMessage && <p className="error">{errorMessage}</p>}
+          <button className="butt" type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
